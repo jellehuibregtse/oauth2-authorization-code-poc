5608516d4b9fbe63762f77f00e2cd55e175287e7
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-11-13 10:24:51 +0100

    Add actuator to API gateway
    Now I can check the routes and health of the gateway

--- api-gateway/src/main/java/com/jellehuibregtse/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jellehuibregtse/apigateway/ApiGatewayApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.http.HttpMethod;
-
-import java.util.Arrays;
 
 /**
  * API Gateway application entry point.
