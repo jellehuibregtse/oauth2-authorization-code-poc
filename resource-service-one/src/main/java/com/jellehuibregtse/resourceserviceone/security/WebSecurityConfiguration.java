058d93cfac1598d3b4ea7af2a49b9bc3e653a36d
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-11-17 18:42:49 +0100

    Fix web configuration

--- resource-service-one/src/main/java/com/jellehuibregtse/resourceserviceone/security/WebSecurityConfiguration.java
+++ resource-service-one/src/main/java/com/jellehuibregtse/resourceserviceone/security/WebSecurityConfiguration.java
@@ -1,31 +1,25 @@
 package com.jellehuibregtse.resourceserviceone.security;
 
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+//        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         http.authorizeRequests()
             .antMatchers("/actuator/**")
             .permitAll()
-            .antMatchers(HttpMethod.GET, "/hamsters/status/check")
-            .hasRole("user")
-            .anyRequest()
+            .antMatchers(HttpMethod.GET, "/**")
             .authenticated()
             .and()
             .oauth2ResourceServer()
-            .jwt()
-            .jwtAuthenticationConverter(jwtAuthenticationConverter);
+            .jwt();
     }
 }
